087625b3b6e4cfffe1ae520cd222d243c3d29837
Kyubey <kyubey@Kyubey.lan>
2023-12-21 00:32:22 -0800

    done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div id="border">
+    <img src="abby-road.jpg" alt="christmas themed characters holding instruments walk across a candy-cane themed crosswalk">
+    <h1>Christmas Sing-Along Songbook</h1>
+    <h2>by Lucas Furseth</h2>
+
+    <h2>Click Anywhere to Download</h2>
+    <!-- <embed src="book.pdf"/> -->
+
+</div>
+
+
+<style>
+
+    embed {
+        width: 100%;
+        height: 100vh;
+    }
+
+    div {
+        font-family: Didot, 'Bodoni MT', 'Noto Serif Display', 'URW Palladio L', P052, Sylfaen, serif;
+font-weight: normal;
+
+        text-align: center;
+        color:rgb(207, 55, 32);
+    }
+    div h1 {
+        font-size: 7vw;
+        font-weight: bold;
+    }
+
+    div h2 {
+        font-size:5vw;
+        font-weight: normal;
+        padding-bottom: 5vw;
+    }
+
+    #border {
+        /* border: 1rem solid red; */
+        border-radius: 25px;
+
+        padding: 1vw;
+        margin: 2vw;
+
+        /* min-height: 100vh; */
+    }
+
+</style>
