8cb7d967b1d8851bb141e92bf4b8b05ba0d34cb1
Kyubey <kyubey@Kyubey.lan>
2023-12-21 02:40:53 -0800

    anchor fallback

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,74 @@
 <div id="border">
-    <img src="abby-road.jpg" alt="christmas themed characters holding instruments walk across a candy-cane themed crosswalk">
-    <h1>Christmas Sing-Along Songbook</h1>
-    <h2>by Lucas Furseth</h2>
+  <img
+    src="abby-road.jpg"
+    alt="christmas themed characters holding instruments walk across a candy-cane themed crosswalk"
+  />
+  <h1>Christmas Sing-Along Songbook</h1>
+  <h2>by Lucas Furseth</h2>
 
-    <h2 id="download">Click On <b>Santa</b> to Download</h2>
-
+  <h2 id="download">Click On <b>Santa</b> to Download</h2>
+  <a id="santa" href="book.pdf"></a>
 </div>
-
 
 <style>
 
-    #download {
-        /* font-weight: bold; */
-        font-family: Superclarendon, 'Bookman Old Style', 'URW Bookman', 'URW Bookman L', 'Georgia Pro', Georgia, serif; 
-        /* font-family: ui-rounded, 'Hiragino Maru Gothic ProN', Quicksand, Comfortaa, Manjari, 'Arial Rounded MT', 'Arial Rounded MT Bo    Ald', Calibri, source-sans-pro, sans-serif;  */
-        /* font-family: Optima, Candara, 'Noto Sans', source-sans-pro, sans-serif;  */
-        /* font-family: Avenir, Montserrat, Corbel, 'URW Gothic', source-sans-pro, sans-serif;  */
-        /* font-family: 'Iowan Old Style', 'Palatino Linotype', 'URW Palladio L', P052, serif;  */
-        /* font-family: Bahnschrift, 'DIN Alternate', 'Franklin Gothic Medium', 'Nimbus Sans Narrow', sans-serif-condensed, sans-serif;  */
-        /* font-family: Rockwell, 'Rockwell Nova', 'Roboto Slab', 'DejaVu Serif', 'Sitka Small', serif; */
+    #santa {
+        width: 35vw;
+        height: 60vw;
+        z-index: 1;
+        position: absolute;
+        top: 0;
+        left: 35vw;
     }
 
-    div {
-        font-family: Didot, 'Bodoni MT', 'Noto Serif Display', 'URW Palladio L', P052, Sylfaen, serif;
-font-weight: normal;
+  #download {
+    /* font-weight: bold; */
+    font-family: Superclarendon, "Bookman Old Style", "URW Bookman",
+      "URW Bookman L", "Georgia Pro", Georgia, serif;
+    /* font-family: ui-rounded, 'Hiragino Maru Gothic ProN', Quicksand, Comfortaa, Manjari, 'Arial Rounded MT', 'Arial Rounded MT Bo    Ald', Calibri, source-sans-pro, sans-serif;  */
+    /* font-family: Optima, Candara, 'Noto Sans', source-sans-pro, sans-serif;  */
+    /* font-family: Avenir, Montserrat, Corbel, 'URW Gothic', source-sans-pro, sans-serif;  */
+    /* font-family: 'Iowan Old Style', 'Palatino Linotype', 'URW Palladio L', P052, serif;  */
+    /* font-family: Bahnschrift, 'DIN Alternate', 'Franklin Gothic Medium', 'Nimbus Sans Narrow', sans-serif-condensed, sans-serif;  */
+    /* font-family: Rockwell, 'Rockwell Nova', 'Roboto Slab', 'DejaVu Serif', 'Sitka Small', serif; */
+  }
 
-        text-align: center;
-        color:rgb(207, 55, 32);
-    }
-    div h1 {
-        font-size: 7vw;
-        font-weight: bold;
-    }
+  div {
+    font-family: Didot, "Bodoni MT", "Noto Serif Display", "URW Palladio L",
+      P052, Sylfaen, serif;
+    font-weight: normal;
 
-    div h2 {
-        font-size:5vw;
-        font-weight: normal;
-        padding-bottom: 5vw;
-    }
+    text-align: center;
+    color: rgb(207, 55, 32);
+    /* pointer-events: none; */
+    user-select: none;
+  }
+  div a {
+    color: rgb(207, 55, 32);
+    text-decoration: none;
+  }
+  div a:hover {
+    color: rgb(207, 55, 32);
+    text-decoration: underline;
+  }
+  div h1 {
+    font-size: 7vw;
+    font-weight: bold;
+  }
 
-    #border {
-        /* border: 1rem solid red; */
-        border-radius: 25px;
+  div h2 {
+    font-size: 5vw;
+    font-weight: normal;
+    padding-bottom: 5vw;
+  }
 
-        padding: 1vw;
-        margin: 2vw;
+  #border {
+    /* border: 1rem solid red; */
+    border-radius: 25px;
 
-        /* min-height: 100vh; */
-    }
+    padding: 1vw;
+    margin: 2vw;
 
+    /* min-height: 100vh; */
+  }
 </style>
